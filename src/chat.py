cca5d76638c940a9f010ff7f34b6b217e49c71a7
spgridd <spajak@griddynamics.com>
2025-05-07 17:58:31 +0200

    refactor: change current structure

--- src/chat.py
+++ src/chat.py
@@ -4,18 +4,21 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-openai_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=openai_key)
+# client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def ask_gpt(prompt):
-    """
-    Ask gpt model given question and receive answer.
-    """
-    response = client.chat.completions.create(
-        model='gpt-4.1-mini',
-        messages=[
-            {'role': 'user', 'content': prompt}
-        ]
-    )
+class ChatClient():
+    def __init__(self):
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-    return response.choices[0].message.content
+    def ask_gpt(self, prompt):
+        """
+        Ask gpt model given question and receive answer.
+        """
+        response = self.client.chat.completions.create(
+            model='gpt-4.1-mini',
+            messages=[
+                {'role': 'user', 'content': prompt}
+            ]
+        )
+
+        return response.choices[0].message.content
--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
-from chat import ask_gpt
+from chat import ChatClient
 
 def main():
+    chat = ChatClient()
+
     print("Welcome! Type your question or type 'exit' to quit.")
     
     while True:
         prompt = input("User: ")
         if prompt.lower() == 'exit':
             break
-        reply = ask_gpt(prompt=prompt)
+        reply = chat.ask_gpt(prompt=prompt)
         print(f"System: {reply}")
 
 
